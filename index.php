70ef9ea7da162829a1426d8362353afd87aabc6f
Marina <42719234+Mgostinceva@users.noreply.github.com>
2021-03-06 16:57:52 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,280 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet" >
+	<link href="css/style-phone.css" rel="stylesheet" >
+	
+    <title>LevelUp</title>
+	</head>
+	<body>
+		
+		<div class="block1">
+			<div id="video">
+				<video class="videos" width="100%" height="auto" preload="auto" autoplay="autoplay"
+				loop="loop">
+				<source src="/video/fon.mp4" type="video/mp4"></source>
+				</video>
+			</div>
+				
+			<div class="container">
+				  
+				  <div class="row">
+						<div class="col name-company"><span class="name-level">Level<span><span class="name-up">Up<span></div>
+				   </div>
+				   
+				  <div class="row justify-content-center block-1">
+						<div class="col-6 img-yeprod">
+							<div class="circle-yellow">
+							<img class="item" src="/image/prod.png">
+							</div>
+						</div>
+						
+						<div class="col-6 property"><div class="zagolovok"> Пусть ничто не упадет на вашем пути!</div>
+							<p class="description">Как принято считать, явные признаки победы институционализации, превозмогая сложившуюся непростую половую ситуацию</p>
+							<p class="description">Как принято считать, явные признаки победы институционализации, превозмогая сложившуюся непростую половую ситуацию</p>
+							<p class="description">Как принято считать, явные признаки победы институционализации, превозмогая сложившуюся непростую половую ситуацию</p>
+							<div class="but-zakaz"><a href="#zakaz"><span class="order">ЗАКАЗАТЬ СЕЙЧАС</span></a></div>
+						</div>
+					</div>
+			</div> 
+		</div>
+		
+		<div class="block2">
+			<div class="backg">
+				<div class="container ingrid">
+				  <div class="row pt-4 align-items-center sostavi">
+				  
+					<div class="col-12 align-self-center"> 
+							<div class="text-ig"><span>Состав продукта</span></div>
+					</div>
+					
+					<div class="col align-self-center phone-margin">
+							
+							<div class="sostav-tf">
+								<div><img class="ingridients" src="/image/belokrylnik-bolotnyy.jpg"></div>
+								<div><span class="name-ingr">Ёлка</span></div>
+								<div class="ingrt"><span class="pod-ingr">Как принято считать, явные признаки победы институции</span></div>
+							</div>
+							
+							<div class="sostav-tf">
+								<div><img class="ingridients" src="/image/belokrylnik-bolotnyy.jpg"></div>
+								<div><span class="name-ingr">Иголка</span></div>
+								<div class="ingrt"><span class="pod-ingr">Как принято считать, явные признаки победы институции</span></div>
+							</div>
+							
+							<div class="sostav-tf">
+								<div><img class="ingridients" src="/image/belokrylnik-bolotnyy.jpg"></div>
+								<div><span class="name-ingr">Плацебо</span></div>
+								<div class="ingrt"><span class="pod-ingr">Как принято считать, явные признаки победы институции</span></div>
+							</div>
+					</div>
+					
+					<div class="col align-self-center phone-margin">
+							<div><img class="it-prod" src="/image/prod.png"></div>
+					</div>
+					
+					<div class="col align-self-center phone-margin">
+							<div class="sostav-tf">
+								<div><img class="ingridients" src="/image/belokrylnik-bolotnyy.jpg"></div>
+								<div><span class="name-ingr">Ёлка</span></div>
+								<div class="ingrt"><span class="pod-ingr">Как принято считать, явные признаки победы институции</span></div>
+							</div>
+
+							<div class="sostav-tf">
+								<div><img class="ingridients" src="/image/belokrylnik-bolotnyy.jpg"></div>
+								<div><span class="name-ingr">Вода</span></div>
+								<div class="ingrt"><span class="pod-ingr">Как принято считать, явные признаки победы институции</span></div>
+							</div>
+
+							<div class="sostav-tf">
+								<div><img class="ingridients" src="/image/belokrylnik-bolotnyy.jpg"></div>
+								<div><span class="name-ingr">Бумага</span></div>
+								<div class="ingrt"><span class="pod-ingr">Как принято считать, явные признаки победы институции</span></div>
+							</div>
+							
+					</div>
+				  </div>
+				</div>	
+			</div>
+		</div>
+		
+		<div class="block3">
+			<div class="backg-pattern">
+			<div class="container pt-5">
+				<h2 class="text-center pt-5 revie-client">Отзывы наших клиентов</h2>
+				<h4 class="text-center pt-2 pb-5 revie-say">Что говорят наши клиенты</h4>
+				<div id="carouselReviews" class="carousel slide" data-ride="carousel">
+				 
+				 <ol class="carousel-indicators">
+					 <li data-target="#carouselReviews" data-slide-to="0" class="active"></li>
+					  <li data-target="#carouselReviews" data-slide-to="1"></li>
+					  <li data-target="#carouselReviews" data-slide-to="2"></li>
+				   </ol>
+				   
+				<div class="carousel-inner">
+					 <div class="carousel-item active">
+					  <div class="row align-items-center revie">
+						   <div class="col align-self-center">
+								<div><img src="https://html-plus.in.ua/wp-content/uploads/2019/01/lara.jpg" class="img-fluid rounded-circle d-block m-auto pb-3" alt="Лариса Уварова"></div>
+								<div class="review">
+								<div class="review-author"><span>Лариса Уварова</span></div>
+								<div class="text-review"><span class="reviewes">В своём стремлении повысить качество жизни, они забывают, что постоянный количественный рост и сфера нашей активности прекрасно подходит для реализации кластеризации усилий. Ясность нашей позиции очевидна: новая модель организационной деятельности говорит о возможностях распределения внутренних резервов и ресурсов.</span> 
+								</div></div>
+							</div>
+						</div>
+					</div>
+						
+					<div class="carousel-item">
+					   <div class="row align-items-center">
+						   <div class="col align-self-center revie">
+								<div><img src="https://html-plus.in.ua/wp-content/uploads/2019/01/lara.jpg" class="img-fluid rounded-circle d-block m-auto pb-3" alt="Лариса Уварова"></div>
+								<div class="review">
+								<div class="review-author"><span>Лариса Уварова</span></div>
+								<div class="text-review"><span class="reviewes">В своём стремлении повысить качество жизни, они забывают, что постоянный количественный рост и сфера нашей активности прекрасно подходит для реализации кластеризации усилий. Ясность нашей позиции очевидна: новая модель организационной деятельности говорит о возможностях распределения внутренних резервов и ресурсов.</span> 
+								</div></div>
+							</div>
+						</div>
+					</div>
+			
+					<div class="carousel-item">
+					   <div class="row align-items-center revie">
+						   <div class="col align-self-center">
+								<div><img src="https://html-plus.in.ua/wp-content/uploads/2019/01/lara.jpg" class="img-fluid rounded-circle d-block m-auto pb-3" alt="Лариса Уварова"></div>
+								<div class="review">
+								<div class="review-author"><span>Лариса Уварова</span></div>
+								<div class="text-review"><span class="reviewes">В своём стремлении повысить качество жизни, они забывают, что постоянный количественный рост и сфера нашей активности прекрасно подходит для реализации кластеризации усилий. Ясность нашей позиции очевидна: новая модель организационной деятельности говорит о возможностях распределения внутренних резервов и ресурсов.</span> 
+								</div></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+					<a class="carousel-control-prev" href="#carouselReviews" role="button" data-slide="prev">
+					   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					   <span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselReviews" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						  <span class="sr-only">Next</span>
+					  </a>
+					  
+				</div> 
+			</div>
+			</div>
+		</div>
+		
+		<div class="block4">
+			<div class="background-pattern py-5">
+				<div class="container pt-5">
+				  <div class="row pt-5 mb-3">
+					<div class="col-sm">
+					  <div class="doctor"><img class="doc" src="/image/doctor2.png"></div>
+					</div>
+					
+					<div class="col-sm">
+							  <div class="text-doctor">
+								<h2 class="py-3 text-doc">В стремлении повысить качество жизни и реализации воздействия!</h2>
+								<p class="haracter">С другой стороны, укрепление и развитие внутренней структуры говорит о возможностях приоритизации разума над эмоциями. Имеется спорная точка зрения, гласящая примерно следующее: реплицированные с зарубежных источников, современные исследования, инициированные исключительно синтетически, разоблачены.</p>
+							  </div>  
+							  <div class="orders-go my-5">
+								<span class="order"><a href="#zakaz"><span class="order">ЗАКАЗАТЬ СЕЙЧАС</span></a></span>
+							  </div>
+					</div>
+				  </div>
+				  
+				  <div class="row icons-text justify-content-center py-5">
+				 	<div class="col pb-5">
+						<div class="icons mb-2"><img src="/image/icons8-капсула-100.png"></div>
+						<span class="te-ic px-1">Эксперты утверждают, что убеждённость некоторых оппонентов оказалась </span>
+					</div>
+					
+					<div class="col pb-5">
+						<div class="icons mb-2"><img src="/image/icons8-person-laying-down-100.png"></div>
+						<span class="te-ic px-1">Нынче никто не может себе позволить инициировать детский</span>
+					</div>
+					
+					<div class="col pb-5">
+						<div class="icons mb-2"><img src="/image/icons8-статистика-100.png"></div>
+						<span class="te-ic px-1">Эксперты утверждают, что убеждённость некоторых оппонентов оказалась</span>
+					</div>					
+
+				   </div>
+				 </div>
+		        </div>
+		     </div>
+		</div>
+		
+		
+		<div id="zakaz" class="block5">
+			<div class="background-pattern">
+				<div class="container pb-5">
+				  <div class="row justify-content-center">
+				  <div class="form-comp"><span>LevelUp</span></div>
+				  <div class="text-formp"><span>Заказать сейчас!</span></div>
+				  </div>
+				 
+				 <div class="row justify-content-center mt-3 mb-5">
+				 <div class="form-time"><script src="//megatimer.ru/get/4ee82585fa1d2098ab5230ea321ad6c5.js"></script></div>
+				  </div>
+				  
+				<div class="row py-3">
+					<div class="col-sm item-price">
+						<div class="circle-yellow">
+							<div class="img-prod"><img class="item-product" src="/image/prod.png"></div>
+						</div>
+					  
+						<div>
+						   <del class="old-price">1590<i class="fa fa-btc" aria-hidden="true"></i></del>
+							<div class="loading">
+							<span class="new-bay">1999₽</span>
+							</div>
+						</div>
+					</div>
+
+				   <div class="col-sm">
+				   
+					  <form class="forms">
+						<label for="fname">Имя</label>
+						<input type="text" id="fname" name="firstname" placeholder="Иван">
+
+						<label for="lname">Фамилия</label>
+						<input type="text" id="lname" name="lastname" placeholder="Иванов">
+
+						
+						<label for="nimber">Номер телефона</label>
+						<input class="number-phone" type="text" name="Номер телефона"  placeholder="+7 (999) 999-99-99">
+
+						 <div class="orders-go my-3">
+								<span class="order">ОТПРАВИТЬ</span>
+						</div>
+						
+					  </form>	
+				   </div>
+				</div>
+				</div>
+			</div>
+		</div>
+		
+		
+		<div class="footer">
+			<div class="backg-foot">
+			<div class="py-3"><span class="sub">© Все права защищены <a class="telega" target="_blank" href="https://t.me/mgostinceva" style="color:#282828" >Gostinceva Marina</a>  2021</span></div>
+			</div>
+		</div>	
+	
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+	integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.5/jquery.inputmask.min.js" integrity="sha512-sR3EKGp4SG8zs7B0MEUxDeq8rw9wsuGVYNfbbO/GLCJ59LBE4baEfQBVsP2Y/h2n8M19YV1mujFANO1yA3ko7Q==" crossorigin="anonymous"></script>
+	
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+  </body>
+</html>
